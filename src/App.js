3160c8671413b14549333ceba954941cc0685296
Yonas-coder <52844663+Yonas-coder@users.noreply.github.com>
2023-04-25 16:11:39 -0400

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
+
 
 function App() {
+  // rpx create-react-app visa-atm-locator
+
+  //variable with your apiKey
+  // const apiKey = "9ae68c5";
+
+  //State to hold movie data
+  const [atms, setAtms] = useState(null);
+
+  //Function to getMovies
+  const getATMs = async (searchTerm) => {
+    // make fetch request and store response
+    try {
+      const response = await fetch(`https://sandbox.api.visa.com/globalatmlocator/v1/localatms/totalsinquiry`,
+
+      );
+      // Parse JSON response into a javascript object
+      const data = await response.json();
+      //set the Movie state to the movie
+      setAtms(data);
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  useEffect(() => {
+    console.log(atms)
+  }, [atms])
+
+  getATMs()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
